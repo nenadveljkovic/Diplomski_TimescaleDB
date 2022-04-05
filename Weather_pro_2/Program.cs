1e755235d43434408df8d8fb275290bf500b2817
nenadveljkovic <nenad.veljkovic98@gmail.com>
2022-04-05 16:28:30 +0200

    Dodat jos jedan projekat koji upisuje meteoroloske podatke u message queue.

---
+++ Weather_pro_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using CsvHelper;
+using Diplomski_TimescaleDB.Models;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Weather_pro_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("conditions",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+            List<WeatherConditions> conditions;
+            string jsonString;
+            using var fileReader = new StreamReader(@"podaci_conditions.csv");
+            using (var csv = new CsvReader(fileReader, CultureInfo.InvariantCulture))
+            {
+                conditions = csv.GetRecords<WeatherConditions>().ToList();
+            }
+            jsonString = JsonConvert.SerializeObject(conditions);
+            var body = Encoding.UTF8.GetBytes(jsonString);
+            channel.BasicPublish("", "conditions", null, body);
+        }
+    }
+}
